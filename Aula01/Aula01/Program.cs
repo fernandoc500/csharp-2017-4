9e5ce575980320c73e884b8ac26ab850c96d2aa1
Fernando <fernando_carmo500@hotmail.com>
2017-10-03 20:28:17 -0300

    first commit

---
+++ Aula01/Aula01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int teste = somar(5, 5);
+
+            Console.Write(teste);
+            Console.ReadLine();
+        }
+
+        static int somar(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
